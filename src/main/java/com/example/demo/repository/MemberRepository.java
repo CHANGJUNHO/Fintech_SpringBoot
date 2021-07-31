50cc8d6269796db344e80f016ea5197ce1a06a9b
Elice <elice@elice.com>
2021-07-31 18:17:46 +0900

    springboot db restful api connection

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends PagingAndSortingRepository<Member, Long> {
 
 }
